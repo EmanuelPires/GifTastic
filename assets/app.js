5936e218fa71bf51003f4e05a2e323604a788755
Emanuel Pires <emanuelpires11@gmail.com>
2018-12-06 03:09:56 -0500

    Stop and start working

--- assets/app.js
+++ assets/app.js
@@ -9,7 +9,7 @@ value and create a button with that value.
 That button value will also be added to a data-value property of the button
 
 */
-var animalSearched = [];
+var animalSearched = ["goats"];
 
 function renderButtons() {
   $(".buttonSpace").empty();
@@ -42,23 +42,72 @@ $(document).on("click", ".animalButton", function() {
   var queryURL =
     "https://api.giphy.com/v1/gifs/search?q=" +
     animal +
-    "&api_key=dc6zaTOxFJmzC&limit=10";
+    "&api_key=qblmO0dj7fRyzxgTFKW8jP8rV14dGMC8&limit=10";
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response);
     var results = response.data;
 
+    $("#gifSpace").empty();
     for (var i = 0; i < results.length; i++) {
+      //
       var animalDiv = $("<div>");
       var p = $("<p>").text("Rating: " + results[i].rating);
       var animalImage = $("<img>");
       animalImage.attr("src", results[i].images.fixed_height_still.url);
+      animalImage.attr("data-state", "still");
+      animalImage.attr("data-id", results[i].id);
       animalDiv.append(p);
       animalDiv.append(animalImage);
       $("#gifSpace").append(animalDiv);
     }
   });
+});
+
+renderButtons();
+//Now we need the function to switch between still and animated.
+//I'm thinking we can use the API ID to retrieve the animated vs still versions of gifs
+$(document).on("click", "img", function() {
+  var state = $(this).attr("data-state");
+  if (state === "still") {
+    var id = $(this).attr("data-id");
+
+    var queryURL =
+      "http://api.giphy.com/v1/gifs/" +
+      id +
+      "?&api_key=qblmO0dj7fRyzxgTFKW8jP8rV14dGMC8";
+    var image = $(this);
+    console.log(image);
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      var results = response.data;
+
+      image.attr("src", results.images.fixed_height.url);
+      image.attr("data-state", "animated");
+    });
+  }
+
+  if (state === "animated") {
+    var id = $(this).attr("data-id");
+
+    var queryURL =
+      "http://api.giphy.com/v1/gifs/" +
+      id +
+      "?&api_key=qblmO0dj7fRyzxgTFKW8jP8rV14dGMC8";
+    var image = $(this);
+    console.log(image);
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      var results = response.data;
+
+      image.attr("src", results.images.fixed_height_still.url);
+      image.attr("data-state", "still");
+    });
+  }
 });
